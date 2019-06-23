17e4283417161f7d61bed5f14c6a0d0783bea009
Jun Liang <illuminatemynight@gmail.com>
2019-06-23 23:33:50 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,23 @@
-# Your Code Here
+def map(source_array)
+  new = []
+  i = 0
+  while i < source_array.length do
+    new.push(yield(source_array[i]))
+    i += 1
+  end
+  return new
+end
+
+def reduce(source_array, starting_point = 0)
+  new = 0
+  i = 0
+  if source_array[i].class == Integer
+    while i < source_array.length do
+      new += source_array[i]
+      i += 1
+    end
+  yield(new, starting_point)
+  else
+    yield(true, false)
+  end
+end
